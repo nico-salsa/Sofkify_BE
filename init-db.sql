41fc28eb99643a5e74918c239733b8d7a8d744be
Santiago Avila <sanavila2002@gmail.com>
2026-02-07 13:23:26 -0500

    Add Docker setup for microservices

---
+++ init-db.sql
@@ -0,0 +1,7 @@
+-- Crear las bases de datos para cada microservicio
+CREATE DATABASE sofkify_users;
+CREATE DATABASE sofkify_products_bd;
+
+-- Dar permisos al usuario postgres
+GRANT ALL PRIVILEGES ON DATABASE sofkify_users TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE sofkify_products_bd TO postgres;
